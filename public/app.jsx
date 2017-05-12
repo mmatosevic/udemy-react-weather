1d8962651a00b335c46833977b5e3f116c72c087
mmatosevic <matija.matosevic@infobip.com>
2017-05-12 23:04:53 +0200

    Manage state and input events

--- public/app.jsx
+++ public/app.jsx
@@ -1,12 +1,32 @@
 var Title = React.createClass({
+    getDefaultProps: function() {
+        return {text: "Untitled"}
+    },
+    getInitialState: function() {
+        return {text: this.props.text}
+    },
+    sendForm: function(e){
+        e.preventDefault();
+        var name = this.refs.name.value;
+        this.setState({
+            text: name
+        });
+    },
     render: function() {
         return (
-            <h2>ReactAPP</h2>
+            <div>
+                <h2>ReactAPP</h2>
+                <p>{this.state.text}</p>
+                <form onSubmit={this.sendForm}>
+                    <input type="text" ref="name"/>
+                    <button>Send</button>
+                </form>
+            </div>
         );
     }
 });
 
 ReactDOM.render(
-    <Title/>,
+    <Title text="Those are mine titles!"/>,
     document.getElementById('app')
 );
