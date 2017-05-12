92a7f1a8a5c7182687ed087c5ad8f97408f81d21
mmatosevic <matija.matosevic@infobip.com>
2017-05-13 00:00:40 +0200

    Component nesting and message passing

--- public/app.jsx
+++ public/app.jsx
@@ -1,37 +1,56 @@
 var Title = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h2>ReactAPP</h2>
+                <p>{this.props.text}</p>
+            </div>
+        );
+    }
+});
+
+var TextInput = React.createClass({
+    submitText: function(e) {
+        e.preventDefault();
+        var nameInput = this.refs.name;
+        var name = nameInput.value;
+        nameInput.value = '';
+        this.props.onNewName(name);
+    },
+    render: function() {
+        return (
+            <form onSubmit={this.submitText}>
+                <input type="text" ref="name"/>
+                <button>Send</button>
+            </form>
+        )
+    }
+});
+
+var Application = React.createClass({
     getDefaultProps: function() {
         return {text: "Untitled"}
     },
     getInitialState: function() {
         return {text: this.props.text}
     },
-    sendForm: function(e){
-        e.preventDefault();
-        var nameInput = this.refs.name;
-        var name = nameInput.value;
-        nameInput.value = '';
-
-        if (typeof name === 'string' && name.length > 0){
-            this.setState({
-                text: name
-            });
-        }
+    handleNewName: function(name){
+        this.setState({
+            text: name
+        });
     },
     render: function() {
         return (
             <div>
-                <h2>ReactAPP</h2>
-                <p>{this.state.text}</p>
-                <form onSubmit={this.sendForm}>
-                    <input type="text" ref="name"/>
-                    <button>Send</button>
-                </form>
+                <Title text={this.state.text}/>
+                <TextInput onNewName={this.handleNewName}/>
             </div>
-        );
+        )
     }
 });
 
+
 ReactDOM.render(
-    <Title text="Those are mine titles!"/>,
+    <Application text="Those are mine titles!"/>,
     document.getElementById('app')
 );
