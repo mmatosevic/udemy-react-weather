133d7ccd998bc1d14b3c471371b893d3500e8dd1
Matija Matošević <matija5513@gmail.com>
2017-05-19 14:25:54 +0200

    Api call works as intended

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var OpenWeatherMap = require('OpenWeatherMap');
 
 var Weather = React.createClass({
     getInitialState: function() {
@@ -10,9 +11,14 @@ var Weather = React.createClass({
         }
     },
     handleLocationUpdate: function(location) {
-        this.setState({
-            location: location,
-            temp: 30
+        var that = this;
+        OpenWeatherMap.getTemp(location).then(function(temp){
+            that.setState({
+                location: location,
+                temp: temp
+            });
+        }, function(err){
+            alert(err);
         });
     },
     render: function() {
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,9 +1,12 @@
 var React = require('react');
 
 var WeatherMessage = React.createClass({
+    toCelsius: function(kelvin){
+        return kelvin - 273.15;
+    },
     render: function() {
         return(
-            <p>It's {this.props.temperature}C in {this.props.location}</p>
+            <p>It's {this.toCelsius(this.props.temperature)}C in {this.props.location}</p>
         )
     }
 });
