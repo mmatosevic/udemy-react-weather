00159dbe5410016cee25f9ebc42caad04b63e902
mmatosevic <matija.matosevic@infobip.com>
2017-05-24 20:56:34 +0200

    Multiline still works!

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,9 @@
 var React = require('react');
 
-var About = (props) => <div><p>About componenet stateless arrow!</p><p>None</p></div>;
+var About = (props) => 
+    <div>
+        <p>About componenet stateless arrow!</p>
+        <p>None</p>
+    </div>;
 
 module.exports = About;
