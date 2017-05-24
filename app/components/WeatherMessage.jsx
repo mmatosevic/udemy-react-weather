a3c7367b2edca3e8e43e5c36437077436695c374
mmatosevic <matija.matosevic@infobip.com>
2017-05-24 21:18:38 +0200

    Use in-arg destructuring

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,11 +1,11 @@
 var React = require('react');
 
-var WeatherMessage = (props) => {
+var WeatherMessage = ({temperature, location}) => {
         var toCelsius = function(kelvin){
             return kelvin - 273.15;
         };
         return(
-            <p>It's {toCelsius(props.temperature)}C in {props.location}</p>
+            <p>It's {toCelsius(temperature)}C in {location}</p>
         )
 };
 
